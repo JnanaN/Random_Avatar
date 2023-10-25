e0a335183f68ba6cfb1d186111e48510837d39f6
JnanaN <jnanabhushan943@gmail.com>
2023-10-25 22:06:44 +0530

    made chnages in the UI

--- generate/urls.py
+++ generate/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("index/" , views.index, name="index"),
-    path("random/" , views.random_avatar, name="random"),
     path("" , views.signup, name="signup"),
     path("login/", views.login_view, name="login"),
-    path("generate/<str:seed>", views.avatar_input, name="avatar")
+    path("index/" , views.index, name="index"),
+    path("generate/<str:seed>", views.avatar_input, name="avatar"),
+    path("random/" , views.random_avatar, name="random"),    
 ]
